1b63833b0da0f823112b7942902a2d84da4ba10b
LucasWines <lucw1991@gmail.com>
2026-05-06 12:38:10 -0600

    The initial CLI menu loop. The first half of Phase 1 has been completed up to this point in this file.

--- TaskManager/src/taskmanager/Main.java
+++ TaskManager/src/taskmanager/Main.java
@@ -1,15 +1,103 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package taskmanager;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.List;
+import java.util.Scanner;
+
+import models.Task;
+import services.TaskService;
+
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        // Task service
+        TaskService service = new TaskService();
+
+        // Tasks list
+        List<Task> tList = service.getTasks();
+
+        // Scanner object for user input
+        Scanner in = new Scanner(System.in);
+
+        // Running boolean for exiting
+        boolean isRunning = true;
+
+        // Boolean to stay or leave option page
+        boolean stay = true;
+
+
+        while (isRunning) {
+
+            // Stay set to true at the main menu each time.
+            stay = true;
+
+            // Main menu
+            System.out.print("Welcome! Begin managing your tasks:\n...\n...\n");
+
+            System.out.println("What do you want to do?");
+            System.out.println("1: Add Task \n2: View Tasks \n3: Exit");
+
+
+            // Take in a choice from the user.
+            int choice = in.nextInt();
+            in.nextLine();
+
+            // Choice navigation
+            if (choice == 1) {
+
+                System.out.println("For task creation, add a name:");
+
+                // Take in user input for name
+                String tName = in.nextLine();
+
+                // Create the new task with the given name
+                service.addTask(tName);
+
+                // Confirm
+                System.out.println("Task added!");
+
+                // Short wait before moving on
+                Thread.sleep(1500);
+
+            } else if (choice == 2) {
+
+                while (stay) {
+                    if (tList.isEmpty()) {
+                        System.out.println("No tasks yet.");
+                        stay = false;
+                    } else {
+                        for (Task t : tList) {
+                            // Use getName to print the name. getName and status in models/Task.java
+                            System.out.println(t.getName() + ", " + t.getStatus());
+                        }
+                        System.out.println("Would you like to go back to the main page? Just Y/N");
+
+                        String ans = in.nextLine();
+                        if (ans.equals("Y") || ans.equals("y") || ans.equals("yes")
+                            || ans.equals("YES") || ans.equals("Yes")) {
+                            stay = false;
+                        } else if (ans.equals("N") || ans.equals("n") || ans.equals("no")
+                                || ans.equals("NO") || ans.equals("No")) {
+                            stay = true;
+                        } else {
+                            System.out.println("Answer must be yes or no.");
+                        }
+                    }
+                }
+
+            } else if (choice == 3) {
+
+                // Exit program
+                System.out.println("Goodbye!");
+                isRunning = false;
+
+            } else {
+                System.out.println("Choose a correct option, 1 through 3.");
+                Thread.sleep(1000);
+            }
+
         }
+
     }
+
 }
