ddd68b53f9f63243bc52cce9d66448652d9fdd77
LucasWines <lucw1991@gmail.com>
2026-05-06 12:37:12 -0600

    This file holds the logic for CRFUD operations. The data gatekeeper.

---
+++ TaskManager/src/services/TaskService.java
@@ -0,0 +1,31 @@
+package services;
+
+import models.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+   This file can be thought of as the gatekeeper to the data for now. It owns the list and is the
+   only class allowed to touch it directly. Any operation that reads or changes the list goes through here.
+
+   This holds the functions for any CRUD operations.
+*/
+
+
+public class TaskService {
+
+    // Array list to store created tasks.
+    private List<Task> taskList = new ArrayList<>();
+
+    public void addTask(String name) {
+        Task task = new Task(name);
+
+        taskList.add(task);
+    }
+
+    public List<Task> getTasks() {
+        return taskList;
+    }
+
+}
