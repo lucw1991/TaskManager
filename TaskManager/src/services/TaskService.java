8065a33085653ffd6398d371a5400805633f0883
LucasWines <lucw1991@gmail.com>
2026-05-07 11:53:12 -0600

    Added in function to update tasks. Also added function to get just one task at a time, rather than the full list, and another to mark tasks complete. Both unused for now.

--- TaskManager/src/services/TaskService.java
+++ TaskManager/src/services/TaskService.java
@@ -1,5 +1,6 @@
 package services;
 
+import enums.Status;
 import models.Task;
 
 import java.util.ArrayList;
@@ -18,14 +19,72 @@ public class TaskService {
     // Array list to store created tasks.
     private final List<Task> taskList = new ArrayList<>();
 
+
+
+
     public void addTask(String name) {
         Task task = new Task(name);
 
         taskList.add(task);
     }
 
+
     public List<Task> getTasks() {
         return taskList;
+    }
+
+
+    public String getTask(String name) {
+        String task = null;
+        boolean found = false;
+
+
+        for (Task t : taskList) {
+            if (t.getName().equals(name)) {
+                task = t.getName();
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Task not found!");
+        }
+
+        return task;
+    }
+
+
+    public void updateTask(String task, Status status) {
+
+        // Track if found. Must be in function or else it will get declared false over and over for the loop.
+        boolean found = false;
+
+        // Iterate through list,
+        for (Task t : taskList) {
+            // checking task names,
+            if (t.getName().equals(task)) {
+                // If found, update status,
+                t.setStatus(status);
+
+                // update found,
+                found = true;
+
+                // break the loop.
+                break;
+            }
+        }
+
+        // Give an error if not found.
+        if (!found){
+            System.out.println("Task does not exist!");
+        }
+
+    }
+
+
+    public void markComplete(String task) {
+        updateTask(task, Status.COMPLETE);
     }
 
 }
