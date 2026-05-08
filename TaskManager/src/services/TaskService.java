6ce2ee1c3cef626ebb3a883a83a25f9cc94f7170
LucasWines <lucw1991@gmail.com>
2026-05-08 08:54:00 -0600

    Added in a deleteTask function and also update the addTask function to filter out duplicate entries and return a boolean to use as a value in main.

--- TaskManager/src/services/TaskService.java
+++ TaskManager/src/services/TaskService.java
@@ -22,36 +22,49 @@ public class TaskService {
 
 
 
-    public void addTask(String name) {
-        Task task = new Task(name);
+    public boolean addTask(String name) {
 
-        taskList.add(task);
-    }
+        // Track if added to keep duplicates out.
+        boolean added = false;
 
-
-    public List<Task> getTasks() {
-        return taskList;
-    }
-
-
-    public String getTask(String name) {
-        String task = null;
+        // Track if found
         boolean found = false;
 
+        // Task object
+        Task task = new Task(name);
 
+        // Check if present already in the list.
         for (Task t : taskList) {
-            if (t.getName().equals(name)) {
-                task = t.getName();
+            // If present then break the look and continue.
+            if (t.getName().equals(task.getName())) {
                 found = true;
                 break;
             }
         }
 
+        // If not found, then add task to the list. If found, then print the message.
         if (!found) {
-            System.out.println("Task not found!");
+            taskList.add(task);
+            added = true;
+        } else {
+            System.out.println("Task already exists!");
         }
 
-        return task;
+        return added;
+
+    }
+
+
+    public boolean deleteTask(String name) {
+
+        return taskList.removeIf(t ->
+                        t.getName().equals(name));
+
+    }
+
+
+    public List<Task> getTasks() {
+        return taskList;
     }
 
 
